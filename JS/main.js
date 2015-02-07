4309ed3f7787590a13f67476dad01bf47a6822b7
N_Dominic <nikhil_s_d@yahoo.co.in>
2015-02-07 02:01:39 -0500

    Call me Ishmael

--- JS/main.js
+++ JS/main.js
@@ -98,12 +98,7 @@ socket.on('serverToClient', function(data)
 function requestProfileFromServer() {
 
 	var profile = {
-		functionName: "requestProfileFromServer",
-		accountId: null,
-		userAge: null,
-		userGender: null,
-		proImg: null,
-		currentLocation: null
+		functionName: "requestProfileFromServer"
 	}
 	
 	clientToServer(profile)
@@ -114,6 +109,14 @@ function requestProfileFromServer() {
 
 function getProfileFromServer(profile) {
 
-	return profile
+	var enemyProfile = {
+		enemyUserName: profile.userName,
+	 	enemyAccountId:  profile.accountId,
+		enemyProImg: profile.proImg,
+		enemyUserAge: profile.userAge,
+		enemyUserGender: profile.userGender,
+		enemyCurrentLocation: profile.currentLocation
+	}
+
 }
 
