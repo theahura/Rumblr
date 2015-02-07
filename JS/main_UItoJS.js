9c3fa5ab0fee49ed8f82c49f32987a56fd66b139
tyler.im <tji2105@columbia.edu>
2015-02-07 05:21:20 -0500

    trying to add functionality to check and x buttons

--- JS/main_UItoJS.js
+++ JS/main_UItoJS.js
@@ -9,6 +9,21 @@ $("#login").click(function()
      login();
 });
 
+$("#circle-left").click(function()
+{
+
+	$("#profileImage").fadeOut("slow")
+	$("#profileImage").fadeIn("slow")
+
+});
+$("#circle-right").click(function()
+{
+
+	$("#profileImage").fadeOut("slow")
+	$("#profileImage").fadeIn("slow")
+
+});
+
 // $("#getLocationFromServer").click(function()
 // {
 //     getLocationFromServer();
