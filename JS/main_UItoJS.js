99545ce1267366b5ba311cea8c2f5c7e6be11cf0
tyler.im <tji2105@columbia.edu>
2015-02-07 10:52:25 -0500

    deleted commented code and console

--- JS/main_UItoJS.js
+++ JS/main_UItoJS.js
@@ -45,7 +45,6 @@ $(".circle-right").click(function()
 
 $("#submitRadius").click(function()
 {
-    console.log($(userProx).val())
     convertInputMilesToDegrees($(userProx).val())
     $('.userRadius').html($(userProx).val());
 });
@@ -55,12 +54,3 @@ $("#getNearbyRumblesButton").click(function()
 	requestNearbyRumbles();
 })
 
-// $("#getLocationFromServer").click(function()
-// {
-//     getLocationFromServer();
-// });
-		  
-// $(.userName).html(userName);
-// $(.userAge).html(userAge);
-// $(.userGender).html(userGender);
-// $(.currentLocation).html(currentLocation);
