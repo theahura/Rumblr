ef712e1a668e438fc57a404cffa2f7703e085d33
Michele <michelelin123@gmail.com>
2015-02-07 06:53:51 -0500

    added email to scope

--- JS/gplus.js
+++ JS/gplus.js
@@ -32,7 +32,7 @@ function login()
     'cookiepolicy' : 'single_host_origin',
     'callback' : 'loginCallback',
     'approvalprompt':'force',
-    'scope' : 'https://www.googleapis.com/auth/plus.login'
+    'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
   };
   gapi.auth.signIn(myParams);
 }
