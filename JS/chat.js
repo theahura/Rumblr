ae5e0ceafe12d64a8a7c45d38c7b0726afc3b2cb
jcbartlett25 <jcbart12595@gmail.com>
2015-02-06 23:58:45 -0500

    Worked on chat.js
    Added a few functions

--- JS/chat.js
+++ JS/chat.js
@@ -10,7 +10,7 @@ Functions needed (note: a lot of these will not be actual functions, but will be
 joinRoom() - used to allow two users (defined by their sockets) to connect to a private room running socket.io in chat_server; generates room key using
 				combination of user names of both people who swipe right
 
-getMessage() - used to grab whatever data users put into their chat box 
+receiveMessage() - used to grab whatever data users put into their chat box 
 
 sendMessage(text) - used to send whatever data users put into their chat box to the global chat thing
 
@@ -38,3 +38,93 @@ Chat div - where all messages are pushed to
 Maintain multiple chat options
 
 */
+
+function sendMessage() {
+	var message = $inputMessage.val(); 
+	message = cleanInput(message); // "Prevent markup from being injected into the message" ??
+	 if (message && connected) {
+      $inputMessage.val(''); 
+      addMessage({
+        userName: userName,
+        message: message,
+        accountId: accountId,
+      });
+      socket.emit('clientToServer', message); // tells server to execute 'new message'
+  }
+}
+
+//Next function will log users' chats
+function logChat(message, options) { 
+	var $el = $('<li>').addClass('log').text(message);
+    addMessageElement($el, options);
+}
+
+// Adds the visual chat message to the message list
+ function addMessage (data, options) {
+    // Don't fade the message in if there is an 'X was typing'
+    var $typingMessages = getTypingMessages(data);
+    options = options || {};
+    if ($typingMessages.length !== 0) {
+      options.fade = false;
+      $typingMessages.remove();
+    }
+
+/* This is additional functionality to dispay "User is typing"
+
+Adds the visual chat typing message
+  function addChatTyping (data) {
+    data.typing = true;
+    data.message = 'is typing';
+    addChatMessage(data);
+  }
+ // Removes the visual chat typing message
+  function updateTyping () {
+    if (connected) {
+      if (!typing) {
+        typing = true;
+        socket.emit('typing');
+      }
+      lastTypingTime = (new Date()).getTime();
+
+      setTimeout(function () {
+        var typingTimer = (new Date()).getTime();
+        var timeDiff = typingTimer - lastTypingTime;
+        if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+          socket.emit('stop typing');
+          typing = false;
+        }
+      }, TYPING_TIMER_LENGTH);
+    }
+  }
+// Gets the 'X is typing' messages of a user
+  function getTypingMessages (data) {
+    return $('.typing.message').filter(function (i) {
+      return $(this).data('userName') === data.userName;
+    });
+  }
+
+ */
+
+// Socket events
+
+ // Whenever the server emits 'new message', update the chat body
+  socket.on('message', function (data) {
+    updateChatMessages(data);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
