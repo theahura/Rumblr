bdd3999ab08653a1f1d4b148a010031b4a1386fe
Ganesh Ravichandran <gr2483@columbia.edu>
2015-02-07 09:21:10 -0500

    sadas

--- Remote/server.js
+++ Remote/server.js
@@ -102,10 +102,10 @@ io.sockets.on('connection', function(socket)
 		hashTable[object.accountId].proImg = object.proImg;
 		hashTable[object.accountId].accountId = object.accountId;
 		hashTable[object.accountId].username = object.userName;
-		hashTable[object.accountId].swipeLeftIDs = object.swipeLeftIDs;
 		hashTable[object.accountId].coordinates = object.coordinates;
 		hashTable[object.accountId].emailAddress = object.emailAddress;
 		hashTable[object.accountId].userGender = object.userGender;
+		hashTable[object.accountId].swipeRightProfile = [];
 	}
 
 	function checkUserRegistration(object) {
@@ -130,14 +130,14 @@ io.sockets.on('connection', function(socket)
 		return hashTable[object.accountId].gameChoices;
 	}
 
-	function storeswipeLeftIDs(object) {
+	function storeSwipeRightProfile(object) {
 		// saves Swipe Right IDs of user
-		hashTable[object.accountId].swipeLeftIDs = object.swipeLeftIDs;
+		hashTable[object.accountId].swipeRightProfile.push(object.enemyProfile);
 	}
 
-	function getswipeLeftIDs(object) {
+	function getSwipeRightProfile(object) {
 		// returns Swipe Right IDs of user
-		return hashTable[object.accountId].swipeLeftIDs;
+		return hashTable[object.accountId].swipeRightProfile;
 	}
 
 	function storeGeoLocation(object) {
